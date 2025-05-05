0009d56e4ba179af36722fe36e48ef04e6bba2c8
MD. Faysal Hossain <faysalhossain4455@gmail.com>
2025-05-05 23:00:24 +0600

    update

---
+++ Jobkhujibd-backend/Jobkhujibd-backend/src/main/java/com/faysal/Jobkhujibd_backend/JobkhujibdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.faysal.Jobkhujibd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobkhujibdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobkhujibdBackendApplication.class, args);
+	}
+
+}
---
+++ Jobkhujibd-backend/Jobkhujibd-backend/src/test/java/com/faysal/Jobkhujibd_backend/JobkhujibdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faysal.Jobkhujibd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobkhujibdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
